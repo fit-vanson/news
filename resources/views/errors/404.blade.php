ecb38b03480c004f284df03963ca105b84d5f049
Administrator <sonvt.vietmmo@gmail.com>
2023-06-24 16:25:24 +0700

    s

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,83 +1,68 @@
-@extends('layouts.frontend')
+@php
+    $siteInfo = getSiteInfo();
+@endphp
+@extends('frontend.master')
+@section('meta_content')
 
-@section('title', __('404 - Not Found'))
-@php $gtext = gtext(); @endphp
+    <meta name="keywords" content="{{ $siteInfo['og_keywords'] }}"/>
+    <meta name="title" content="{{ $siteInfo['og_title'] }}">
+    <meta name="description" content="{{ $siteInfo['og_description'] }}"/>
+    <meta property="og:title" content="{{ $siteInfo['og_title'] }}"/>
+    <meta property="og:site_name" content="{{ $siteInfo['site_name'] }}"/>
+    <meta property="og:description" content="{{ $siteInfo['og_description'] }}"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="{{ url()->current() }}"/>
+    <meta property="og:image" content="{{ asset('media/'.$siteInfo['og_image']) }}"/>
+    <meta property="og:image:width" content="600"/>
+    <meta property="og:image:height" content="315"/>
 
-@section('meta-content')
-	<meta name="keywords" content="{{ $gtext['og_keywords'] }}" />
-	<meta name="description" content="{{ $gtext['og_description'] }}" />
-	<meta property="og:title" content="{{ $gtext['og_title'] }}" />
-	<meta property="og:site_name" content="{{ $gtext['site_name'] }}" />
-	<meta property="og:description" content="{{ $gtext['og_description'] }}" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="{{ url()->current() }}" />
-	<meta property="og:image" content="{{ asset('media/'.$gtext['og_image']) }}" />
-	<meta property="og:image:width" content="600" />
-	<meta property="og:image:height" content="315" />
-	@if($gtext['fb_publish'] == 1)
-	<meta name="fb:app_id" property="fb:app_id" content="{{ $gtext['fb_app_id'] }}" />
-	@endif
-	<meta name="twitter:card" content="summary_large_image">
-	@if($gtext['twitter_publish'] == 1)
-	<meta name="twitter:site" content="{{ $gtext['twitter_id'] }}">
-	<meta name="twitter:creator" content="{{ $gtext['twitter_id'] }}">
-	@endif
-	<meta name="twitter:url" content="{{ url()->current() }}">
-	<meta name="twitter:title" content="{{ $gtext['og_title'] }}">
-	<meta name="twitter:description" content="{{ $gtext['og_description'] }}">
-	<meta name="twitter:image" content="{{ asset('media/'.$gtext['og_image']) }}">
 @endsection
+@section('main_content')
+    <main class="main">
+        <!-- Page Breadcrumb -->
+        <div class="breadcrumb-section">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{ url('/') }}">{{ __('Home') }}</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">{{ __('404 - Not Found') }}</li>
+                            </ol>
+                        </nav>
+                    </div>
 
-@section('header')
-@include('frontend.partials.header')
-@endsection
+                </div>
+            </div>
+        </div>
+        <!-- /Page Breadcrumb/ -->
 
-@section('content')
-
-<main class="main">
-	<!-- Page Breadcrumb -->
-	<div class="breadcrumb-section">
-		<div class="container">
-			<div class="row align-items-center">
-				<div class="col-lg-6">
-					<nav aria-label="breadcrumb">
-						<ol class="breadcrumb">
-							<li class="breadcrumb-item"><a href="{{ url('/') }}">{{ __('Home') }}</a></li>
-							<li class="breadcrumb-item active" aria-current="page">{{ __('404 - Not Found') }}</li>
-						</ol>
-					</nav>
-				</div>
-				<div class="col-lg-6">
-					<div class="page-title">
-						<h1>{{ __('404 - Not Found') }}</h1>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- /Page Breadcrumb/ -->
-
-	<!-- Inner Section -->
-	<section class="inner-section inner-section-bg">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 offset-md-3">
-					<div class="error_card">
-						<div class="error_img">
-							<img src="{{ asset('frontend/images/404.png') }}" />
-						</div>
-						<p>{{ __('This page you are looking for could not be found!') }}</p>
-						<a class="btn theme-btn" href="{{ url('/') }}">{{ __('Back to Home Page') }}</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-	<!-- /Inner Section/ -->
-</main>
+        <!-- Inner Section -->
+        <section class="inner-section inner-section-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 offset-md-3">
+                        <div class="error_card">
+                            <div class="error_img">
+                                <img src="{{ asset('frontend/img/404.png') }}" />
+                            </div>
+                            <p>{{ __('This page you are looking for could not be found!') }}</p>
+                            <a class="btn theme-btn" href="{{ url('/') }}">{{ __('Back to Home Page') }}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- /Inner Section/ -->
+    </main>
 
 @endsection
 
-@push('scripts')
 
-@endpush
+
+
+
+
+
+
+
