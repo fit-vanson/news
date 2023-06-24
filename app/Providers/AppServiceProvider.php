60d1d2ba9a68d9bc2f2b7c335abe219e6d1a1a4c
Administrator <sonvt.vietmmo@gmail.com>
2023-06-24 11:43:27 +0700

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,6 +30,6 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
 
 		Paginator::useBootstrap();
-//        URL::forceScheme('https');
+        URL::forceScheme('https');
     }
 }
